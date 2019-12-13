d7234d28ac2d6f4bbc737b86e76ac30961cfe891
mingmonth <ys9922@naver.com>
2019-12-13 09:52:38 +0900

    체크 언체크 토글 처리

--- test1.cpp
+++ test1.cpp
@@ -48,37 +48,56 @@ int check_state2() {
 	return -1;
 }
 
+// check uncheck toggle
+void setCheckValue() {
+	if(a[num1][num2].state1 == 0) {
+		a[num1][num2].state1 = 1;	
+	} else {
+		a[num1][num2].state1 = 0;	
+	}
+}
+
 // check uncheck
+// input 값이 입력되는 순간 전체 데이터에 반영
 void check_state1() {
 	int checkedIndex = -1;
-	// 체크되었던 index 얻어오기.	
-	checkedIndex = check_state2();
-	printf("checked Index:[%d]\n", checkedIndex);
+	// 체크되었던 index 얻어오기.
+	// 토글 하기 이전 check 상태인 인덱스 값 얻어오기. 
+	checkedIndex = check_state2();	
+	// 토글 액션 
+	setCheckValue();		
+	
 	for(int i = 0; i < LED_NUM_MAX; i++) {
-		for(int j = 0; j < RADIO_BTN_NUM_MAX; j++) {			
-			
+		for(int j = 0; j < RADIO_BTN_NUM_MAX; j++) {
+									
 			if(i == num1) {
-				// uncheck
-				a[i][j].state1 = 0;
-				if(j == num2) {
-					// check
-					a[i][j].state1 = 1;					
+				if(j != num2) {
+					// reset all
+					a[i][j].state1 = 0;
 				}
 			} else {
-				if(j == num2) {					
-					//disable
-					a[i][j].state2 = 1;
-				} else if(j == checkedIndex) {
+				if(j == num2) {
+					// 기준 값이 check 상태면 disable
+					// 기준 값이 uncheck 상태면 enable
+					if(a[num1][num2].state1 == 1)  {
+						a[i][j].state2 = 1;
+					} else {
+						a[i][j].state2 = 0;
+					}
+				} 
+				// check 되어있었던 index 값은 enable 상태로 변경 
+				else if(j == checkedIndex) {
 					//enable
 					a[i][j].state2 = 0;
 				}
-			}			
+			}		
 		}
 	}
 	printArray();
 }
 
 // 1:true, 0:false
+// disable 된 값을 고르는 경우 다시 선택 받도록 한다. 
 int check_enable() {	 
 	if(a[num1][num2].state2 == 1) {
 		return 0;
@@ -117,9 +136,7 @@ int check_range2() {
 void select_radio_button() {
 	if(break_flag)	{
 		return;
-	}
-	
-	printArray();
+	}	
 	
 	do {
 		printf("STEP2-input radio button number:");
@@ -131,6 +148,8 @@ void select_led() {
 	do {
 		printf("STEP1-input panel number:");
 		scanf("%d", &num1);
+		// led가 선택되었을때는 이미 반영된 데이터 확인
+		printArray();
 	} while(!check_range1());
 }
 
