796cd02307672f53bfaaff0373b6f1bed9d4f6ee
김윤수 <yskim@taeha.co.kr>
2020-03-09 11:16:16 +0900

    스레드 뮤텍스2

--- test_thread01.cpp
+++ test_thread01.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
-void worker(int& counter) {
+void worker(int& result, std::mutex& m) {
 	for(int i = 0; i < 10000; i++) {
-		counter += 1;
+		//m.lock();
+		std::lock_guard<std::mutex> lock(m);
+		result += 1;
+		//m.unlock();
 	}
 }
 
 int main() {
 	int counter = 0;
+	std::mutex m;
 	
 	std::vector<std::thread> workers;
-	for(int i = 0; i < 100; i++) {
-		workers.push_back(std::thread(worker, std::ref(counter)));
+	for(int i = 0; i < 4; i++) {
+		workers.push_back(std::thread(worker, std::ref(counter), std::ref(m)));
 	}
 	
-	for(int i = 0; i < 100; i++) {
+	for(int i = 0; i < 4; i++) {
 		workers[i].join();
 	}
 	
